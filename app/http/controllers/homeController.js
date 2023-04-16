0a838f86cfd8ee24f7d7d0fe8563980debf7df9d
Shubham Kr <72254047+shubhamkr83@users.noreply.github.com>
2023-04-16 08:07:25 +0530

    push the project

---
+++ app/http/controllers/homeController.js
@@ -0,0 +1,12 @@
+const Menu = require('../../models/menu')
+function homeController() {
+    return {
+        async index(req, res) {
+            const foods = await Menu.find();
+            console.log(foods);
+            return res.render('home', { foods: foods })
+        }
+    }
+}
+
+module.exports = homeController
